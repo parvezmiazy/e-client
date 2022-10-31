0cfb71515870694159f6a99098a0a240662d7fdb
Parvez Ahmed <miazyparvez@gmail.com>
2022-10-31 23:49:22 +0600

    Carosel and loading indicator added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PaginationModule } from 'ngx-bootstrap';
+import { CarouselModule, PaginationModule } from 'ngx-bootstrap';
 import { PaginationHeaderComponent } from './components/pagination-header/pagination-header.component';
 import { PaginationFooterComponent } from './components/pagination-footer/pagination-footer.component';
 @NgModule({
   declarations: [PaginationHeaderComponent, PaginationFooterComponent],
-  imports: [CommonModule, PaginationModule.forRoot()],
+  imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
   exports: [
     PaginationModule,
     PaginationHeaderComponent,
     PaginationFooterComponent,
+    CarouselModule,
   ],
 })
 export class SharedModule {}
